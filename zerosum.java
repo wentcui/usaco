81221d67882767d84547fcef956648adde23457c
wentcui <wentcui@gmail.com>
2013-12-15 22:37:05 -0800

    zerosum_a

---
+++ zerosum.java
@@ -0,0 +1,16 @@
+/*
+ID: wentcui1
+LANG: JAVA
+TASK: lamps
+ */
+import java.io.*;
+import java.util.*;
+
+class zerosum {
+	public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("zerosum.in"));
+        out = new PrintWriter(new BufferedWriter(new FileWriter("zerosum.out")));
+        StringTokenizer st = new StringTokenizer(f.readLine());
+		int n = Integer.parseInt(st.nextToken());
+	}
+}
