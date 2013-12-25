745e4bbad6019eb67814099fd1cc8a32cae80fd3
wentcui <wentcui@gmail.com>
2013-12-24 22:57:24 -0800

    agrinet

--- agrinet.cpp
+++ agrinet.cpp
@@ -1,4 +1,7 @@
 /*
+ID: wentcui1
+LANG: C++
+TASK: agrinet
 */
 
 #include <iostream>
@@ -14,30 +17,36 @@ struct vnode{
 	int b;
 	int v;
 };
-struct vnode* varr[10000];
+struct vnode varr[10000];
 
 int compare(const void *x, const void *y) {
-	return ((struct vnode *)x)->v - ((struct vnode *)y)->v;
+	struct vnode *_x = (struct vnode*)x;
+	struct vnode *_y = (struct vnode*)y;
+	return _x->v - _y->v;
 }
 
-int dfs(int p, int **resmatrix, int* visited) {
+int dfs(int p, int **resmatrix, int* visited, int step) {
 	int i;
-	if (visited[p])
+	if (visited[p]) {
+		if (step - visited[p] == 2)
+			return 0;
 		return 1;
-	visited[p] = 1;
+	}
+	visited[p] = step;
 	for(i = 1; i <= n; i++) {
-		if (resmatrix[p][i] < ITF && dfs(i, resmatrix, visited))
+		if (resmatrix[p][i] < ITF && dfs(i, resmatrix, visited, step + 1))
 			return 1;
 	}
+	visited[p] = 0;
 	return 0;
 }
 
 int checkloop(int **resmatrix) {
-	int i;
+	int i, step = 1;
 	int *visited = (int*)malloc(sizeof(int) * (n + 1));
 	memset(visited, 0, sizeof(int) * (n + 1));
 	for(i = 1; i <= n; i++) {
-		if (!visited[i] && dfs(i, resmatrix, visited))
+		if (!visited[i] && dfs(i, resmatrix, visited, step))
 			return 1;
 	}
 	return 0;
@@ -47,12 +56,9 @@ main() {
 	FILE* fin = fopen("agrinet.in", "r");
 	FILE* fout = fopen("agrinet.out", "w");
 	int i, j, pos = 0;
-	printf("dddddddddddddd\n");
 	fscanf(fin, "%d", &n);
-	printf("readin: %d\n", n);
 	int **matrix = (int**)malloc(sizeof(int*) * (n + 1));
 	int **resmatrix = (int**)malloc(sizeof(int*) * (n + 1));
-	printf("hi1\n");
 	for(i = 0; i <= n; i++) {
 		matrix[i] = (int *)malloc(sizeof(int) * (n + 1));
 		resmatrix[i] = (int *)malloc(sizeof(int) * (n + 1));
@@ -65,34 +71,35 @@ main() {
 		for(j = 1; j <= n; j++) {
 			fscanf(fin, "%d", &matrix[i][j]);
 			if (i < j) {
-				struct vnode *n = (struct vnode *)malloc(sizeof(struct vnode));
-				n->a = i;
-				n->b = j;
-				n->v = matrix[i][j];
-				printf("%d %d %d\n", i, j, matrix[i][j]);
-				varr[pos++] = n;
+				varr[pos].a = i;
+				varr[pos].b = j;
+				varr[pos].v = matrix[i][j];
+				pos++;
 			}
 		}
-		printf("\n");
 	}
 
-	qsort(varr, pos, sizeof(struct vnode *), compare);
-	printf("hi2\n");
-	if (!varr) {
-		printf("WTF\n");
-		return 1;
-	}
+	qsort(varr, pos, sizeof(struct vnode), compare);
 
 	i = 1;
 	j = 0;
+	int sum = 0;
 	while(i < n) {
-		struct vnode *n = varr[j++];
+		struct vnode *n = &varr[j++];
 		printf("%d %d %d\n", n->a, n->b, n->v);
+		sum += n->v;
 		resmatrix[n->a][n->b] = n->v;
+		resmatrix[n->b][n->a] = n->v;
 		if(checkloop(resmatrix)) {
+			sum -= n->v;
+			printf("loop: %d %d\n", n->a, n->b);
 			resmatrix[n->a][n->b] = ITF;
+			resmatrix[n->b][n->a] = ITF;
 			continue;
 		}
 		i++;
 	}
+	fprintf(fout, "%d\n", sum);
+	fclose(fout);
+	exit(0);
 }
