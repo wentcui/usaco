b4ceceaa8443e62766cc89643ebcced1a0a31c84
wentcui <wentcui@gmail.com>
2013-12-26 14:59:55 -0800

    humble

--- humble.java
+++ humble.java
@@ -12,28 +12,39 @@ class humble {
 		BufferedReader fin = new BufferedReader(new FileReader("humble.in"));
 		PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("humble.out")));
 		StringTokenizer st = new StringTokenizer(fin.readLine());
-		HashMap<Long, Boolean> existed = new HashMap<Long, Boolean>();
-		long cur;
 		int k = Integer.parseInt(st.nextToken());
 		int n = Integer.parseInt(st.nextToken());
-		int[] set = new int[k];
+		int[] set = new int[k + 1];
+		int[] rec = new int[k + 1];
+		long[] dp = new long[n + 2];
+		int cur = 2;
 		st = new StringTokenizer(fin.readLine());
-		for(int i = 0; i < k; i++) {
+		for(int i = 1; i <= k; i++) {
 			set[i] = Integer.parseInt(st.nextToken());
-			existed.put((long)set[i], true);
+			rec[i] = 1;
 		}
-		existed.put((long)1, true);
-
-		for(cur = 2; n > 0; cur++) {
-			for(int j = 0; j < k; j++) {	
-				if (cur >= set[j] && (cur % set[j] == 0) && existed.containsKey(cur / set[j])) {
-					existed.put(cur, true);
-					n--;
-					break;
+		dp[0] = (long)1;
+		dp[1] = (long)1;
+		for(int i = 0; i < n; i++) {
+			long minv = Long.MAX_VALUE;
+			int minp = 1;
+			for(int j = 1; j <= k; j++) {
+				long nextv = dp[rec[j]] * set[j];
+				if (nextv < minv) {
+					minp = j;
+					minv = nextv;
 				}
 			}
+			if (minv <= dp[cur - 1]) {
+				rec[minp]++;
+				i--;
+				continue;
+			}
+			dp[cur++] = minv;
+			rec[minp]++;
 		}
-		fout.println(cur - 1);
+
+		fout.println(dp[n + 1]);
 		fout.close();
 		System.exit(0);
 	}
