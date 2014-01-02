44705d937d5e2640da47ef8e3220de366e6b79f3
wentcui <wentcui@gmail.com>
2014-01-01 20:47:35 -0800

    stamps_b

--- stamps.c
+++ stamps.c
@@ -14,49 +14,33 @@ main() {
 
 	int K, N, i, j, k, l, max = 0;
 	fscanf(fin, "%d %d", &K, &N);
-	int values[10001];
-	memset(values, 0, sizeof(int) * (10001));
-	int** dp = (int **)malloc(sizeof(int *) * (K + 1));
+	int values[51];
+	memset(values, 0, (sizeof(int) * 51));
 	for(i = 1; i <= N; i++) {
 		fscanf(fin, "%d", &values[i]);
 		if (max < values[i])
 			max = values[i];
 	}
-	for(i = 0; i <= K; i++) {
-		dp[i] = malloc(sizeof(int) * (max * K + 1));
-		memset(dp[i], 0, sizeof(int) * (max * K + 1));
-	}
-	printf("s\n");
-
-	for(i = 1; i <= K; i++) {
-		dp[i][0] = 1;
-	}
-	for(i = 1; i <= N; i++) {
-		dp[1][values[i]] = 1;
-	}
-	printf("s1\n");
-
-	for(i = 1; i <= N; i++) {
-		for(j = 2; j <= K; j++) {
-			for(k = 1; k <= max * K; k++) {
-				int v = 0;
-				for(l = 0; l <= j && k - l * values[i] >= 0; l++) {
-					v |= dp[j - l][k - l * values[i]];
-				}
-				dp[j][k] = v;
+	int* dp = (int *)malloc(sizeof(int) * (max * K + 1));
+    memset(dp, 0, sizeof(int) * (max * K + 1));
+    dp[0] = 1;
+	values[0] = 1;
+    for(i = 1; i <= K; i++) {
+        for(j = i * max; j > 0; j--) {
+        	int v = 0;
+            for(k = N; k >= 0; k--) {
+                if (j >= values[k])
+                    v |= dp[j - values[k]];
+				if (v) break;
 			}
+			dp[j] = v;
 		}
 	}
 
-	for(k = 1; k <= max * K; k++) {
-		for(i = 1; i <= K; i++) {
-			if (dp[i][k])
-				break;
-		}
-		if (i > K)
+	for(i = 1; i <= K * max; i++) {
+		if (!dp[i])
 			break;
 	}
-
-	printf("Final: %d\n", k - 1);
+	fprintf(fout, "%d\n", i - 1);
 	exit(0);
 }
