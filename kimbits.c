92a21fe4d2ef9a486572b846ad2761a578d3b6bb
wentcui <wentcui@gmail.com>
2014-01-02 23:47:14 -0800

    kimbits

--- kimbits.c
+++ kimbits.c
@@ -8,60 +8,36 @@ TASK: kimbits
 #include <stdlib.h>
 #include <string.h>
 
+#define ITF 2147483647
 
 main() {
 	FILE* fin = fopen("kimbits.in", "r");
 	FILE* fout = fopen("kimbits.out", "w");
-	int N, L, I;
+	int N, L;
 	int i, j, k;
 	int highest = 0, temphighest;
-	unsigned int ret = 0;
-	fscanf(fin, "%d %d %d\n", &N, &L, &I);
-	int* dp[32];
-	int rec[32];
-	for(i = 0; i <= 31; i++) {
-		dp[i] = (int*)malloc(sizeof(int) * (L + 1));
-		memset(dp[i], 0, sizeof(int) * (L + 1));
+	unsigned int ret = 0, I;
+	fscanf(fin, "%d %d %u\n", &N, &L, &I);
+	unsigned int* dp[33];
+	unsigned int rec[33];
+	for(i = 0; i <= 32; i++) {
+		dp[i] = (unsigned int*)malloc(sizeof(unsigned int) * (N + 1));
+		memset(dp[i], 0, sizeof(unsigned int) * (N + 1));
 	}
-
-	dp[1][1] = 1;
-	for(i = 1; i <= 31; i++) {
-		dp[i][0] = 1;
-		rec[i] = 1;
-	}
-	rec[1] = 2;
-	for(i = 2; i <= 31; i++) {
-		for(j = 1; j <= i && j <= L; j++) {
+	for(i=0; i<=N; i++)
+		dp[0][i] = dp[i][0] = 1;
+	for(i = 1; i <= N; i++) {
+		for(j = 1; j <= N; j++) {
 			dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
-			rec[i] += dp[i][j];
-		}
-		if (rec[i] > I) {
-			highest = i;
-			break;
 		}
 	}
-
-	temphighest = highest;
-	while(I && L) {
-		while(rec[highest] > I)
-			highest--;
-		if(rec[highest] == I) {
-			while(highest && L) {
-				ret |= (1 << (highest - 1));
-				L--;
-				highest--;
-			}
-			break;
-		} else {
-			ret |= (1 << highest);
-			I -= rec[highest];
-			L--;
-		}
-	}
-	printf("%d, %d\n", ret, temphighest);
-	while(temphighest > 0) {
-		fprintf(fout, "%d", (ret & (1 << (temphighest - 1))) > 0);
-		temphighest--;
+	for(i=N; i>=1; i--) {
+        if(I>dp[i-1][L]){
+			fprintf(fout, "1");
+            I -= dp[i-1][L];
+            --L;
+        }else
+			fprintf(fout, "0");
 	}
 	fprintf(fout, "\n");
 	exit(0);
