a2ccc77e393b7f3b2216438f620021917ca0b2fe
wentcui <wentcui@gmail.com>
2014-01-09 15:26:51 -0800

    range

--- range.cpp
+++ range.cpp
@@ -22,18 +22,6 @@ int compare(const void* x, const void* y) {
 }
 
 
-bool validate1(int matrix[251][251], int row, int f, int t) {
-	bool ret = true;
-	while(f <= t) {
-		if (!matrix[row][f]) {
-			ret = false;
-			break;
-		}
-		f++;
-	}
-	return ret;
-}
-
 bool validate2(int matrix[251][251], int col, int f, int t) {
 	bool ret = true;
 	while(f <= t) {
@@ -74,34 +62,12 @@ int main() {
 			dp[i][j] = matrix[i][j];
 		}
 	}
-/*
-	for(i = 1; i <= N; i++) {
-		for(j = 1; j <= N; j++) {
-			printf("%d", matrix[i][j]);
-		}
-		printf("\n");
-	}
-*/
+
 	for(k = 2; k <= N; k++)
 	for(i = N; i >= k; i--)
-	for(j = k; j <= N; j++) {
-		if (!matrix[i][j]) {
-			dp[i][j] = 0;
-			continue;
-		}
-
-		//printf("k: %d, i: %d, j: %d, ", k, i, j);
-		bool v = dp[i - 1][j - 1] && ((dp[i - 1][j] && dp[i][j - 1]) || (validate1(matrix, i, j - k + 1, j - 1) && validate2(matrix, j, i - k + 1, i - 1)));
-		//printf("v1: %d, ", v);
-		v |= (dp[i][j - 1] && ((dp[i - 1][j] && dp[i - 1][j - 1]) || (validate1(matrix, i - k + 1, j - k + 1, j - 1) && validate2(matrix, j, i - k + 1, i - 1))));
-		//printf("v2: %d, ", v);
-		v |= (dp[i - 1][j] && ((dp[i][j - 1] && dp[i - 1][j - 1]) || (validate1(matrix, i, j - k + 1, j - 1) && validate2(matrix, j - k + 1, i - k + 1, i - 1))));
-		//printf("v3: %d, ", v);
-		v |= (dp[i][j] && matrix[i - k + 1][j - k + 1] && ((dp[i - 1][j] && dp[i][j - 1]) || ((validate1(matrix, i - k + 1, j - k + 2, j) && validate2(matrix, j - k + 1, i - k + 2, i)))));
-		//printf("v4: %d\n", v);
-
-		dp[i][j] = v;
-		if (v)
+	for(j = N; j >= k; j--) {
+		dp[i][j] &= dp[i - 1][j] && dp[i][j - 1] && dp[i - 1][j - 1];
+		if (dp[i][j])
 			result[k]++;
 	}
 
