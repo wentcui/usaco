93940ef6b39fc6abc6212986121f8d42c5e2b3a6
wentcui <wentcui@gmail.com>
2014-01-11 14:29:18 -0800

    fence8

--- fence8.cpp
+++ fence8.cpp
@@ -5,43 +5,79 @@ TASK: fence8
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <algorithm>
+
+#define ITF 2000000000
 
 using namespace std;
 
+int N, R, maxwaste = 0;
+int boards[51] = {0};
+int rails[1024] = {0};
+
+int compare (const void * a, const void * b)
+{
+  return ( *(int*)a - *(int*)b );
+}
+
+bool dfs(int curr,  int start, int curwaste) {
+	int i, waste = curwaste;
+	bool ret = false;
+	if (curr == 0) {
+		return true;
+	}
+
+	if (waste > maxwaste) {
+		return false;
+	}
+
+	for(i = start; i <= N; i++) {
+		if (boards[i] >= rails[curr]) {
+			boards[i] -= rails[curr];
+
+			if (boards[i] < rails[1]) {
+				waste = curwaste + boards[i];
+			} else
+				waste = curwaste;
+
+			if (rails[curr] == rails[curr - 1])
+				start = i;
+			else
+				start = 1;
+
+			ret |= dfs(curr - 1, start, waste);
+			boards[i] += rails[curr];
+			if (ret)
+				return ret;
+		}
+	}
+	return ret;
+}
+
 int main() {
-	printf("\nfsdfd\n");
 	FILE* fin = fopen("fence8.in", "r");
 	FILE* fout = fopen("fence8.out", "w");
 	int i, j, k;
-	int boards[51] = {0};
-	int rails[1024] = {0};
-	printf("fsdfd\n");
-	int dp[128][51][129] = {{{0}}};
-	int N, R;
 
-	printf("fsdfd\n");
 	fscanf(fin, "%d", &N);
-	for(i = 1; i <= N; i++)
+	for(i = 1; i <= N; i++) {
 		fscanf(fin, "%d", &boards[i]);
+		maxwaste += boards[i];
+	}
 	fscanf(fin, "%d", &R);
-	for(i = 1; i <= R; i++)
-		fscanf(fin, "%d", &rails[i]);
-	printf("fsdfd2\n");
-
 	for(i = 1; i <= R; i++) {
-		for(j = 1; j <= N; j++) {
-			for(k = 1; k <= boards[j]; k++) {
-				if (k < rails[i]) {
-					dp[i][j][k] = max(dp[i - 1][j][k], dp[i][j - 1][boards[j - 1]]);
-				} else {
-					dp[i][j][k] = max(dp[i - 1][j][k], max(dp[i - 1][j][k - rails[i]], dp[i - 1][j - 1][boards[j - 1]]) + 1);
-				}
-				
-			}
-		}
+		fscanf(fin, "%d", &rails[i]);
 	}
 
-	printf("%d\n\n", dp[R][N][boards[N]]);
+	qsort(rails, R + 1, sizeof(int), compare);
+	qsort(boards, N + 1, sizeof(int), compare);
+
+	for(i = 1; i <= R; i++) {
+		maxwaste = maxwaste - rails[i];
+		if (!dfs(i, 1, 0))
+			break;
+	}
+	fprintf(fout, "%d\n", i - 1);
 	return 0;
 }
