423609856a08da70614fb85d92e4900a01447507
wentcui <wentcui@gmail.com>
2014-01-20 21:13:11 -0800

    job_b

--- job.cpp
+++ job.cpp
@@ -37,6 +37,7 @@ int main() {
 		fscanf(fin, "%d", &machineA[0][i]);
 	for(i = 1; i <= M2; i++)
 		fscanf(fin, "%d", &machineB[0][i]);
+	fclose(fin);
 	qsort(machineA[0], M1, sizeof(int), compare);
 	qsort(machineB[0], M2, sizeof(int), compare);
 	left = N;
@@ -77,6 +78,57 @@ int main() {
 	for(i = 1; i <= t; i++) {
 		printf("%d ", time[i]);
 	}
+	printf("\n");
+
+	int at = 0;
+	left = N; // number of tasks that didn't put into machines
+	for(t = 1, at = 1; !time[t]; t++, at++);
+
+	i = 1;
+	printf("%d, %d, %d\n", t, at, time[t]);
+	while (time[t] > 0 && i <= M2) {
+		printf("%d\n", i);
+		machineB[1][i] = machineB[0][i];
+		time[t]--;
+		left--;
+		i++;
+	}
+/*
+
+	while (1) {
+		at++;
+		priority_queue<PP, vector<PP>, cmp> q;
+		vector<int> available;
+		
+		for (i = 1; i <= M2; i++) {
+			if (machineB[1][i] > 0)
+				machineB[1][i]--;
+
+			if (machineB[1][i])
+				q.push(make_pair(i, machineB[1][i]));
+			else
+				available.push_back(i);
+		}
+		if (available.size() == M2 && left <= 0)
+			break;
+		while(t < 20001 && !time[t] && t < at)
+			t++;
+		for(i = 0; i < available.size(); i++) {
+			if (time[t] > 0) {
+				time[t]--;
+				left--;
+			} else if (q.top().second > machineB[0][available[i]]) {
+				machineB[1][q.top().first] = 0;
+				q.pop();
+			} else
+				break;
+			machineB[1][available[i]] = machineB[0][available[i]];
+		}
+	}
+	printf("final: %d\n", at);
+	return 0;
+*/
+	fclose(fout);
 	return 0;
 }
 
