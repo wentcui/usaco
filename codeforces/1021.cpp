9ae0428ba8f7e87976c8b91598c0ad43a0f6593d
wentianc <wentianc@amazon.com>
2014-03-11 14:33:06 -0700

    codeforces 1021

--- codeforces/1021.cpp
+++ codeforces/1021.cpp
@@ -4,55 +4,64 @@
 
 using namespace std;
 
-int base, len;
-char input[20];
-unsigned long dp[20][(1 << 16) + 1] = {{0}};
+int base, len, K;
+char input[21];
+unsigned long dp[21][(1 << 16) + 1] = {{0}};
+bool map[21][(1 << 16) + 1] = {{false}};
 
 int getnextmod(int cur, int mod, int curindex) {
-	int shift = len - curindex, i, res = cur % mod;
+	int shift = len - curindex, i, res = cur % K;
 	for(i = 1; i <= shift; i++) {
-		res = (res * base) % mod;
+		res = (res * base) % K;
 	}
-	return mod - res;
+	return mod - res >= 0 ? mod - res : mod - res + K;
 }
 
 unsigned long dfs(unsigned int stat, int mod, int curindex) {
 	unsigned mask = (1 << len) - 1;
 	unsigned int init = (1 << (len - 1)), i, flipstat = (~stat) & mask;
-	unsigned long sum = 0;
+	unsigned long sum = 0, temp;
 
-	printf("*%d %d\n", mod, flipstat);
-	if (dp[mod][flipstat] > 0)
-		return dp[flipstat][mod];
+//	printf("*%d %d, %d\n", mod, flipstat, curindex);
+	if (map[mod][flipstat])
+		return dp[mod][flipstat];
 
 	if (curindex == len) {
 		for(i = 1; i <= len && !(flipstat & init); i++)
 			init = (init >> 1);
 
-		printf("c: %d, i: %d\n", input[i - 1], i - 1);
-		if (!(input[i - 1] % mod)) {
+		map[mod][flipstat] = true;
+		if ((input[i - 1] % K) == (mod % K)) {
+			//printf("yes\n");
 			dp[mod][flipstat] = 1;
 			return 1;
 		}
+	//	printf("no\n");
 		return 0;
 	}
 
 	for(i = 1; i <= len; i++) {
-		if (flipstat & init)
-			sum += dfs(stat | init, getnextmod(input[i - 1], mod, curindex), curindex + 1);
+		if (flipstat & init) {
+			temp = dfs(stat | init, getnextmod(input[i - 1], mod, curindex), curindex + 1);
+			sum += temp;
+			//printf("ret: %lu, stat: %d, mod: %d\n", temp, stat | init, getnextmod(input[i - 1], mod, curindex));
+		}
 		init = (init >> 1);
 	}
 	dp[mod][flipstat] = sum;
-	printf("%d %d: %lu\n", mod, flipstat, sum);
+	map[mod][flipstat] = true;
+	//printf("%d %d: %lu\n", mod, flipstat, sum);
 	return sum;
 }
 
 int main() {
-	int cases, caseno = 0, K, i;
+	int cases, caseno = 0, i;
 	scanf("%d", &cases);
 	while(cases--) {
-		for(i = 0; i < 20; i++)
+		for(i = 0; i < 21; i++) {
 			memset(dp[i], 0, sizeof(unsigned long) * ((1 << 16) + 1));
+			memset(map[i], false, sizeof(bool) * ((1 << 16) + 1));
+		}
 		memset(input, '\0', sizeof(char) * 20);
 
 		scanf("%d %d", &base, &K);
