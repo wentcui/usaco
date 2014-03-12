c42f58354f48bbb39aacbeaba06da2143145aa56
wentianc <wentianc@amazon.com>
2014-03-12 14:31:03 -0700

    codeforces 1025

--- codeforces/1025.c
+++ codeforces/1025.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 unsigned long fn(char *str) {
-	int len = strlen(str), dp[61][61] = {{0}}, i, j, k;
-	unsigned long sum;
+	int len = strlen(str + 1), i, j, k;
+	unsigned long sum, dp[61][61] = {{0}};
 	for(i = 1; i <= len; i++)
 		dp[i][i] = 1;
 
@@ -12,13 +12,14 @@ unsigned long fn(char *str) {
 		for(j = 1; j <= len - i + 1; j++) {
 			sum = dp[j][j + i - 2];
 			for(k = j; k < j + i - 1; k++) {
-				if (str[k - 1] != str[j + i - 2])	continue;
-				else if (k + 1 <= j + i - 2)
-					sum += dp[k + 1][j + i - 2];
-				else
+				if (str[k] != str[j + i - 1])	continue;
+				else if (k + 1 <= j + i - 2) {
+					sum += (dp[k + 1][j + i - 2] + 1);
+				} else {
 					sum += 1;
+				}
 			}
-			dp[j][j + i - 1] = sum + len;
+			dp[j][j + i - 1] = sum + 1;
 		}
 	}
 	return dp[1][len];
@@ -30,8 +31,8 @@ int main() {
 	scanf("%d", &cases);
 	while(cases--) {
 		memset(input, '\0', sizeof(char) * 61);
-		scanf("%s", input);
-		printf("Case %d: %lu", ++caseno, fn(input));
+		scanf("%s", input + 1);
+		printf("Case %d: %lu\n", ++caseno, fn(input));
 	}
 	return 0;
 }
