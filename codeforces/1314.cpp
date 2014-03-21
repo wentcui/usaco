c8f5022eb3a679c4476bd22c8de86aebf9bd96b7
wentcui <wentcui@gmail.com>
2014-03-20 23:51:53 -0700

    codeforces 1314

--- codeforces/1314.cpp
+++ codeforces/1314.cpp
@@ -1,12 +1,25 @@
 #include <stdio.h> 
+#include <stdlib.h>
 #include <string.h>
 #include <vector>
+
+using namespace std;
+
+int P, Q;
 
 struct suffix_t {
 	char v;
 	int level;
 	vector<struct suffix_t *> childs;
 };
+
+int min(int a, int b) {
+	return a > b ? b : a;
+}
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
 
 struct suffix_t *get_node(vector<struct suffix_t *> childs, char v) {
 	for(int i = 0; i < childs.size(); i++) {
@@ -37,13 +50,39 @@ int insert_and_count(struct suffix_t *root, char *str) {
 
 int build_and_count(char *str) {
 	struct suffix_t *root = NULL;
-	int len = strlen(str);
+	int len = strlen(str), sum = 0, curlen, ret, v;
+	if (len < P)
+		return 0;
 	root = (struct suffix_t *)malloc(sizeof(struct suffix_t));
 	if (!root)
 		return 0;
 	root->v = 0;
 	root->level = 0;
-	for(int i = 0; i < str; i++) {
-		
+	for(int i = 0; i < len; i++) {
+		curlen = len - i;
+		if (curlen < P)
+			break;
+		ret = insert_and_count(root, str + i);
+		v = min(curlen, Q) - max(curlen - ret, P);
+		if (v < 0)
+			continue;
+		sum += (v + 1);
 	}
+	return sum;
 }
+
+int main() {
+	int cases, caseno = 0;
+	char str[10001];
+	scanf("%d", &cases);
+	while(cases--) {
+		memset(str, '\0', sizeof(char) * 101);
+		scanf("%s", str);
+		scanf("%d %d", &P, &Q);
+		printf("Case %d: %d", ++caseno, build_and_count(str));
+	}
+	return 0;
+}
+
+
+
