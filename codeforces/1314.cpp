2323054f75da90fd3d2449a0734f9d920a45eada
wentcui <wentcui@gmail.com>
2014-03-22 22:41:59 -0700

    codeforces 1314

--- codeforces/1314.cpp
+++ codeforces/1314.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -21,6 +22,20 @@ int max(int a, int b) {
 	return a > b ? a : b;
 }
 
+void free_nodes(struct suffix_t *root) {
+	struct suffix_t *cur;
+	queue<struct suffix_t *> q;	
+	q.push(root);
+	while(!q.empty()) {
+		cur = q.front();
+		q.pop();
+		for(int i = 0; i < cur->childs.size(); i++) {
+			q.push(cur->childs[i]);
+		}
+		free(cur);
+	}
+}
+
 struct suffix_t *get_node(vector<struct suffix_t *> childs, char v) {
 	for(int i = 0; i < childs.size(); i++) {
 		if (childs[i]->v == v)
@@ -34,28 +49,31 @@ int insert_and_count(struct suffix_t *root, char *str) {
 	int ret = 0;
 	if (*str == '\0')
 		return 0;
+
 	node = get_node(root->childs, *str);
 	if (!node) {
 		ret = 1;
 		node = (struct suffix_t *)malloc(sizeof(struct suffix_t));
 		if (!node)
 			return 0;
+		memset(node, 0, sizeof(struct suffix_t));
 		node->v = *str;
 		node->level = root->level + 1;
 		(root->childs).push_back(node);
 	}
-
 	return ret + insert_and_count(node, str + 1);
 }
 
-int build_and_count(char *str) {
+long long int build_and_count(char *str) {
 	struct suffix_t *root = NULL;
-	int len = strlen(str), sum = 0, curlen, ret, v;
+	int len = strlen(str), curlen, ret, v;
+	long long int sum = 0;
 	if (len < P)
 		return 0;
 	root = (struct suffix_t *)malloc(sizeof(struct suffix_t));
 	if (!root)
 		return 0;
+	memset(root, 0, sizeof(struct suffix_t));
 	root->v = 0;
 	root->level = 0;
 	for(int i = 0; i < len; i++) {
@@ -63,11 +81,19 @@ int build_and_count(char *str) {
 		if (curlen < P)
 			break;
 		ret = insert_and_count(root, str + i);
+		if (!ret)
+			continue;
+		//printf("%s, ret: %d, curlen: %d, Q: %d, P: %d\n", str + i, ret, curlen, Q, P);
 		v = min(curlen, Q) - max(curlen - ret, P);
+		//printf("%d, %d, v: %d\n", min(curlen, Q), max(curlen - ret, P), v);
 		if (v < 0)
 			continue;
-		sum += (v + 1);
+		sum += v;
+		if (curlen - ret < P)
+			sum++;
+		//printf("%llu\n", sum);
 	}
+	free_nodes(root);
 	return sum;
 }
 
@@ -79,10 +105,7 @@ int main() {
 		memset(str, '\0', sizeof(char) * 101);
 		scanf("%s", str);
 		scanf("%d %d", &P, &Q);
-		printf("Case %d: %d", ++caseno, build_and_count(str));
+		printf("Case %d: %lld\n", ++caseno, build_and_count(str));
 	}
 	return 0;
 }
-
-
-
