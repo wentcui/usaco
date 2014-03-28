7b58f6abdd5173ecf3cac2260239e224b37e2402
wentcui <wentcui@gmail.com>
2014-03-27 23:12:14 -0700

    codeforces 1034

--- codeforces/1034.cpp
+++ codeforces/1034.cpp
@@ -1,36 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
-int get_head(int *id, int index) {
-	int pid = 0;
-	if (!id[index])
-		return index;
-	pid = get_head(id, id[index]);
-	id[index] = pid;
-	return pid;
+int N, M;
+int dfn[10001], low[10001], belong[10001];
+int step, group;
+vector<int> graph[10001];
+stack<int> st;
+
+void tarjan(int u) {
+	int v;
+	dfn[u] = low[u] = step++;
+	st.push(u);
+	for(int i = 0; i < graph[u].size(); i++) {
+		v = graph[u][i];
+		if (!dfn[v]) {
+			tarjan(v);
+			low[u] = min(low[u], low[v]);
+		} else if (!belong[v]) {
+			low[u] = min(low[u], dfn[v]);
+		}
+	}
+	if (dfn[u] == low[u]) {
+		do {
+			v = st.top();
+			st.pop();
+			belong[v] = group;
+		} while (v != u);
+		group++;
+	}
+}
+
+int get_nr() {
+	int ret = group - 1, cg, vg, v;
+	int temp[10001];
+	memset(temp, 0, sizeof(int) * 10001);
+	for(int i = 1; i <= N; i++) {
+		cg = belong[i];
+		for(int j = 0; j < graph[i].size(); j++) {
+			v = graph[i][j];
+			vg = belong[v];
+			if (vg == cg)	continue;
+			if (!temp[vg])
+				ret--;
+			temp[vg]++;
+		}
+	}
+	return ret;
 }
 
 int main() {
 	int cases, caseno = 0;
-	int N, M, id[10001], f, t, count, pid;
+	int f, t;
 	scanf("%d", &cases);
 	while(cases--) {
 		scanf("%d %d", &N, &M);
-		memset(id, 0, sizeof(int) * 10001);
-		count = N;
+
+		step = group = 1;
+		memset(dfn, 0, sizeof(int) * 10001);
+		memset(low, 0, sizeof(int) * 10001);
+		memset(belong, 0, sizeof(int) * 10001);
 		for(int i = 1; i <= M; i++) {
 			scanf("%d %d", &f, &t);
-			if (!id[t]) {
-				pid = get_head(id, f);
-				if (pid == t)
-					continue;
-				id[t] = f;
-				count--;
-			}
+			graph[f].push_back(t);
 		}
-		printf("Case %d: %d\n", ++caseno, count);
+		for(int i = 1; i <= N; i++) {
+			if (!belong[i])
+				tarjan(i);
+		}
+		printf("Case %d: %d\n", ++caseno, get_nr());
+		for(int i = 0; i <= N; i++) {
+			graph[i].clear();
+		}
 	}
+	return 0;
 }
