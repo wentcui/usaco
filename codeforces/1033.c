932d1e3f651624b032145ddaa2e6247603d7c8ea
wentcui <wentcui@gmail.com>
2014-03-30 00:43:46 -0700

    codeforces 1033

--- codeforces/1033.c
+++ codeforces/1033.c
@@ -6,37 +6,52 @@
 
 int fn(char *rec) {
 	int dp[204][204] = {{0}};
-	int len = strlen(rec), mid, left, min = MAX_V;
-	for(int i = 2; i <= len; i++) {
-		for(int j = 1; j <= i; j++) {
+	int len = strlen(rec), mid, left, min = MAX_V, min_i;
+	int i, j, lc = 0, rc = 0;
+	for(i = 2; i <= len; i++) {
+		for(j = 1; j < i; j++) {
 			mid = i / 2;
-			dp[i][j] = dp[i - 1][j] + 1
+			dp[i][j] = dp[i - 1][j] + 1;
 			if (j <= mid)	continue;
 
 			left = 2 * j - i;
 			if (rec[left - 1] == rec[i - 1])
 				dp[i][j] -= 2;
 		}
+		dp[i][j] = i - 1;
 	}
 	for(int i = 1; i <= len; i++) {
-		if (dp[N][i] < min)
-			min = dp[N][i];
+		if (dp[len][i] < min) {
+			min = dp[len][i];
+			min_i = i;
+		}
 	}
-	return min;
+	if (min_i != (len + 1) / 2) {
+		if (rec[min_i - 1] == '#') {
+			lc = (min_i - 1) / 2;
+			rc = (len - min_i) / 2;
+		} else {
+			lc = (min_i - 1) / 2 + 1;
+			rc = (len - min_i) / 2 + 1;
+		}
+	}
+
+	return min - abs(lc - rc);
 }
 
 int main() {
 	int cases, caseno = 0, len;
-	char input[101], rec[204]
+	char input[101], rec[204];
 	scanf("%d", &cases);
 	while(cases--) {
 		memset(input, '\0', sizeof(char) * 101);
 		memset(rec, '\0', sizeof(char) * 204);
 		scanf("%s", input);
 		len = strlen(input);
-		for(int i = 0; i < len; i++) {
+		rec[0] = '#';
+		for(int i = 1; i <= len; i++) {
+			rec[2 * i - 1] = input[i - 1];
 			rec[2 * i] = '#';
-			rec[2 * i + 1] = input[i];
 		}
 		printf("Case %d: %d\n", ++caseno, fn(rec));
 	}
